c14e76c85994ebaff71c9def51b8c8095bf30651
David Olea <david@Davids-MacBook-Air.local>
2021-09-26 16:21:54 -0700

    added questions function with twrite file for prompt function

--- Develop/index.js
+++ Develop/index.js
@@ -1,87 +1,109 @@
 // TODO: Include packages needed for this application
 
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type:'input',
-        name:'Title',
-        message:'What is your title? (Required)',
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
+
+const questions = function(questionData){
+    return inquirer
+    .prompt(
+    [
+        {
+            type:'input',
+            name:'title',
+            message:'What is your title? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a title.")
+                    return false;
+                };
             }
-            else {
-                console.log("Please enter a title.")
-                return false;
-            };
-        }
-    },
-    {
-        type:'input',
-        name:'Purpose',
-        message:'What is the purpose of your project? (Required)',
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
+        },
+        {
+            type:'input',
+            name:'Purpose',
+            message:'What is the purpose of your project? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please give a descirption.")
+                    return false;
+                };
             }
-            else {
-                console.log("Please give a descirption.")
-                return false;
-            };
-        }
-    },
-    {
-        type:'input',
-        name:'Built with',
-        message:'What was it built with?'
-    },
-    {
-        type:'input',
-        name:'Installation',
-        message:'How do you Install it?'
-    },
-    {
-        type:'input',
-        name:'Usage',
-        message:'How do you use it?'
-    },
-    {
-        type:'checkbox',
-        name:'Licenses',
-        message:'What License has been applied?',
-        choices:['MIT','GNU GPLv3','GNU LGPLv3', 'GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense']
-    },
-    {
-        type:'input',
-        name:'Contributors',
-        message:'Name of Contributors'
-    },
-    {
-        type:'input',
-        name:'Weblink',
-        message:'What is the link to your website?'
-    },
-    {
-        type:'input',
-        name:'Github name',
-        message:'Github name to redirect to your Github profile'
-    },
-    {
-        type:'input',
-        name:'Email',
-        message:'What is your email?'
-    },
+        },
+        {
+            type:'input',
+            name:'Built with',
+            message:'What was it built with?'
+        },
+        {
+            type:'input',
+            name:'Installation',
+            message:'How do you Install it?'
+        },
+        {
+            type:'input',
+            name:'Usage',
+            message:'How do you use it?'
+        },
+        {
+            type:'checkbox',
+            name:'Licenses',
+            message:'What License has been applied?',
+            choices:['MIT','GNU GPLv3','GNU LGPLv3', 'GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense']
+        },
+        {
+            type:'input',
+            name:'Contributors',
+            message:'Name of Contributors'
+        },
+        {
+            type:'input',
+            name:'Weblink',
+            message:'What is the link to your website?'
+        },
+        {
+            type:'input',
+            name:'Github',
+            message:'Github name to redirect to your Github profile'
+        },
+        {
+            type:'input',
+            name:'Email',
+            message:'What is your email?'
+        },
     
-];
+    ]);
+}
+
+questions()
+    .then(questionData =>{
+        const pageMD = generateMarkdown(questionData)
+        fs.writeFile('./README.md', pageMD, err => {
+            if (err) throw new Error(err);
+            console.log('Page created!');
+        })
+    })
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile() 
+   
+}
 
 // Function call to initialize app
-init();
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,5 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -11,8 +12,10 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(email, github) {
+  return `
+  # ${email.email}
+  # ${github}
 
 `;
 }
